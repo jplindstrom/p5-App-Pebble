61ab4fea07dd85be2474933ab573d6f0297e8945
Johan Lindstrom <johanl@McJpL.local>
2010-12-03 14:39:08 +0000

    pebble.pl

--- source/bin/pebble.pl
+++ source/bin/pebble.pl
@@ -5,6 +5,13 @@ use warnings;
 use Getopt::Long;
 use IO::Pipeline;
 
+use lib "lib";
+use App::Pebble::Object;
+
+no warnings "once";
+*P = *App::Pebble::Object;
+*p = *pmap;
+
 main();
 
 sub main {
@@ -12,12 +19,14 @@ sub main {
         "default_pre:s"  => \( my $default_pre = 'pmap { chomp; $_ }' ),
         "default_post:s" => \( my $default_post = 'pmap { "$_\n" }' ),
     );
-    
-    
-    my ($perl, @files) = @ARGV;
+
+    my ($perl) = @ARGV;
     $perl ||= 'pmap { $_ }';
     my @pipes = grep { $_ } ( q{\*STDIN}, $default_pre, $perl, $default_post, q{\*STDOUT} );
-    
+
     eval join( " | ", @pipes );
     $@ and die;
 }
+
+
+# Dec 02 02:48:35  [68] kCGErrorIllegalArgument: CGXSetWindowFilter: Invalid filter 2
