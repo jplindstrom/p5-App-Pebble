b5fafc6a17e2be572ff946e76ea4ca4684cb111e
Johan Lindstrom <johanl@McJpL.local>
2010-12-03 18:44:31 +0000

    Commands, --parser

--- source/bin/pebble.pl
+++ source/bin/pebble.pl
@@ -8,6 +8,10 @@ use IO::Pipeline;
 use lib "lib";
 use aliased "App::Pebble::Object" => "P";
 
+
+#TODO: plugin system
+use aliased "App::Pebble::Command::df" => "df";
+
 no warnings "once";
 *p = *pmap;
 
@@ -17,11 +21,15 @@ sub main {
     GetOptions(
         "default_pre:s"  => \( my $default_pre = 'pmap { chomp; $_ }' ),
         "default_post:s" => \( my $default_post = 'pmap { "$_\n" }' ),
+        "parser:s"       => \( my $parser ),
     );
+
+    my $parser_perl;
+    $parser and $parser_perl = "$parser->parser";
 
     my ($perl) = @ARGV;
     $perl ||= 'pmap { $_ }';
-    my @pipes = grep { $_ } ( q{\*STDIN}, $default_pre, $perl, $default_post, q{\*STDOUT} );
+    my @pipes = grep { $_ } ( q{\*STDIN}, $default_pre, $parser_perl, $perl, $default_post, q{\*STDOUT} );
 
     eval join( " | ", @pipes );
     $@ and die;
