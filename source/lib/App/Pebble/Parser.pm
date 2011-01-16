b08fa6d3a36f0e245d086d22f6a4e2897f32b0dd
Johan Lindstrom <jpl@nospam>
2011-01-16 22:59:14 +0000

    Refactor: extract base class

--- source/lib/App/Pebble/Parser.pm
+++ source/lib/App/Pebble/Parser.pm
@@ -13,37 +13,14 @@ Parsers always go in the stream, never inside p {} or o {}.
 
 package App::Pebble::Parser;
 use Moose;
+extends "App::Pebble::PluginLoader";
 use Method::Signatures;
 
 use Exporter 'import';
-use Class::Autouse;
 
 sub package_prefix       { "App::Pebble::Plugin::Parser" }
 sub package_abbreviation { "P" }
 
-method load_plugins($class:) {
-    my $package_prefix       = $class->package_prefix;
-    my $package_abbreviation = $class->package_abbreviation;
-
-    # Yeah, I know. Need autouse_recursive to return the loaded
-    # packages.
-    Class::Autouse->autouse_recursive( $package_prefix );
-    my %short_long = map {
-        my $short = $_;
-        $short =~ s/^$package_prefix/$package_abbreviation/;
-        
-        $short => $_;
-    } Class::Autouse::_child_classes( $package_prefix );
-
-    for my $short ( keys %short_long ) {
-        my $long = $short_long{ $short };
-        no strict "refs";
-        *{$short} = sub { $long };
-    }
-
-    our @EXPORT = ( keys %short_long );
-}
-
-BEGIN { __PACKAGE__->load_plugins() }
+our @EXPORT = __PACKAGE__->load_plugins();
 
 1;
---
+++ source/lib/App/Pebble/PluginLoader.pm
@@ -0,0 +1,40 @@
+
+=head1 NAME
+
+App::Pebble::PluginLoader - Base class for plugin loaders, e.g. Parsers.
+
+=cut
+
+package App::Pebble::PluginLoader;
+use Moose;
+use Method::Signatures;
+
+use Class::Autouse;
+
+sub package_prefix       { die( "Abstract\n" ) }
+sub package_abbreviation { die( "Abstract\n" ) }
+
+method load_plugins($class:) {
+    my $package_prefix       = $class->package_prefix;
+    my $package_abbreviation = $class->package_abbreviation;
+
+    # Yeah, I know. Need autouse_recursive to return the loaded
+    # packages.
+    Class::Autouse->autouse_recursive( $package_prefix );
+    my %short_long = map {
+        my $short = $_;
+        $short =~ s/^$package_prefix/$package_abbreviation/;
+        
+        $short => $_;
+    } Class::Autouse::_child_classes( $package_prefix );
+
+    for my $short ( keys %short_long ) {
+        my $long = $short_long{ $short };
+        no strict "refs";
+        *{$short} = sub { $long };
+    }
+
+    return keys %short_long;
+}
+
+1;
