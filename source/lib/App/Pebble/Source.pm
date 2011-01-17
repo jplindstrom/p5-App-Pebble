4e3390c2d0a4c8ced22dc45524db2a6036d19851
Johan Lindstrom <jpl@nospam>
2011-01-17 18:35:53 +0000

    Source::Duration

--- source/lib/App/Pebble/Source.pm
+++ source/lib/App/Pebble/Source.pm
@@ -13,9 +13,11 @@ use MooseX::Method::Signatures;
 use App::Pebble::Source::Web;
 use App::Pebble::Source::XPath;
 use App::Pebble::Source::DateTime;
+use App::Pebble::Source::Duration;
 
 sub Web { "App::Pebble::Source::Web" }
 sub XPath { "App::Pebble::Source::XPath" }
 sub DateTime { "App::Pebble::Source::DateTime" }
+sub Duration { "App::Pebble::Source::Duration" }
 
 1;
