ff8007eee29123d9a3ae0d2272e36fd92c2ab30c
Johan Lindstrom <jpl@nospam>
2011-01-18 00:09:46 +0000

    parse_iso

--- source/lib/App/Pebble/Plugin/Source/DateTime.pm
+++ source/lib/App/Pebble/Plugin/Source/DateTime.pm
@@ -28,4 +28,16 @@ method parse($class: $dt_string) {
     return DateTimeX::Easy->new( $dt_string );
 }
 
+method parse_iso($class: $dt_string) {
+    $dt_string =~ /(\d+)-(\d+)-(\d+) (\d+):(\d+):(\d+)/ or return undef;
+    return DateTime->new(
+        year   => $1,
+        month  => $2,
+        day    => $3,
+        hour   => $4,
+        minute => $5,
+        second => $6,
+    );
+}
+
 1;
