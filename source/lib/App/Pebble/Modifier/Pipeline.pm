cc0e67beed0cfe0c6ae61285ef996910f670140b
Johan Lindstrom <jpl@nospam>
2011-01-19 01:38:17 +0000

    pprogress

--- source/lib/App/Pebble/Modifier/Pipeline.pm
+++ source/lib/App/Pebble/Modifier/Pipeline.pm
@@ -23,9 +23,12 @@ our @EXPORT = qw(
     plimit
     pevery
     pn
+    pprogress
 );
 
 use IO::Pipeline;
+use DateTime;
+use DateTime::Duration;
 
 {
     no warnings "once";
@@ -50,6 +53,40 @@ sub pevery (&) {
 
 sub pn () {
     return pmap { "$_\n" };
+}
+
+sub pprogress (&) {
+    my ($limit_subref) = @_;
+    #$limit_subref->();
+
+    my $count = 0;
+    my $start_time = time();
+    my $start_dt = DateTime->now();
+    my $last_time = $start_time;
+    return pmap {
+        $count++;
+
+        my $now = time();
+        if( $now - $last_time >= 1 ) {
+            my $now_dt = DateTime->now();
+            my $duration = $now_dt - $start_dt;
+            my $duration_text = join( ":", map { $duration->$_ } qw/ hours minutes seconds / );
+
+            my $duration_s = $now - $start_time;
+            my $objects_per_s = sprintf( "%0.1f", $count / ( $duration_s || 1 ) );
+            
+            my $progress = "$count $duration_text [$objects_per_s/s]";
+
+            local $\ = undef;
+            print STDERR "\r$progress";
+
+        }
+
+        $last_time = $now;
+
+        $_;
+    }
+
 }
 
 1;
