67c173ce4532722a35ae87077e01bfdf1e417c3a
Johan Lindstrom <jpl@nospam>
2011-01-31 16:29:28 +0000

    Rename args in Parser::CSV

--- source/lib/App/Pebble/Plugin/Parser/CSV.pm
+++ source/lib/App/Pebble/Plugin/Parser/CSV.pm
@@ -14,15 +14,14 @@ use Text::CSV_XS;
 use App::Pebble::Modifier::Pipeline;
 use Pebble::Object::Class;
 
-method parse($class: :$csv_args, :$fields, :$parse_fields = 0) {
-    $fields || $parse_fields or croak( "P::CSV requires either a list of 'fields' or 'parse_fields' to be specified");
-    $fields ||= [];
+method parse($class: :$csv_args, :$fields, :$has_header = 0) {
+    $fields || $has_header or croak( "P::CSV->parse requires either a list of 'fields' or 'has_header' to be specified" );
 
     $csv_args->{quote_char} ||= q/"/;
     my $csv = Text::CSV_XS->new( $csv_args );
 
     my $line_count = 0;
-    my $seen_field_names = 0;
+    my $has_seen_header = 0;
     return pmap {
       my $line = $_;
       $line_count++;
@@ -36,14 +35,15 @@ method parse($class: :$csv_args, :$fields, :$parse_fields = 0) {
       };
       my @field_values  = $csv->fields;
 
-      if( ( !$seen_field_names ) && $parse_fields ) {
-          $fields = [ map {
+      if( ( !$has_seen_header ) && $has_header ) {
+          $fields ||= [ map {
               s/^\s+//;    # Remove leading whitespace
               s/s+$//;     # Remove trailing whitespace
               s/\W+/_/gsm; # Replace invalid variable name chars with _
               $_,
           } @field_values ];
-          $seen_field_names = 1;
+          $has_seen_header = 1;
+          return ();
       }
 
       my %field_value = map { $_ => shift( @field_values ) } @$fields;
