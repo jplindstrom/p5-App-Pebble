d7fc938b87db86a5d74a1032fbe78d0a019c459d
Johan Lindstrom <jpl@nospam>
2011-01-31 15:35:33 +0000

    Renderer::SQL pretty done

--- source/lib/App/Pebble/Plugin/Renderer/SQL.pm
+++ source/lib/App/Pebble/Plugin/Renderer/SQL.pm
@@ -14,6 +14,27 @@ use IO::Pipeline;
 
 method insert($class: :$table, :$columns?) {
     $columns && ref( $columns ) ne "ARRAY" and $columns = [ $columns ];
+    ###TODO: statement_terminator = ";"
+    ###TODO: statement_separator = "";  # can be "\n\n"
+    ###TODO: sql_dialect, or DBD or somethign to provide good defaults
+    ###for quoting tables, column names and values
+
+    my $column_quoted = {};
+    @$columns = map {
+        if( s/ ( \w+ ) \s* (\[ \s* (\w+) \s* \])/$1/x ) {
+            my $datatype = $3;
+            $datatype ne "Num" and $column_quoted->{ $_ } = 1;
+            ###TODO: needs a lot more work obviously, but this way we
+            ###get DateTime, etc quoted properly
+        }
+        $_;
+    } @$columns;
+
+    my $quoted_sub = sub {
+        my ($attribute, $value) = @_;
+        $column_quoted->{ $attribute } and return qq/"$value"/;
+        return $value;
+    };
 
     return pmap {
         my ($pebble) = @_;
@@ -24,7 +45,12 @@ method insert($class: :$table, :$columns?) {
         ];
         #TODO: validate field names
 
-        my $statement = "INSERT INTO $table (" . join( ", ", @$columns ) . ") VALUES ()";
+        my $statement = "INSERT INTO $table ("
+            . join( ", ", @$columns )
+            . ") VALUES ("
+            . join( ", ", map { $quoted_sub->( $_ => $pebble->$_ ) } @$columns )
+            ###TODO: escape values
+            . ");";
 
         $statement;
     };
