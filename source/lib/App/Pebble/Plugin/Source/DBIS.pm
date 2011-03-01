50e6c1fbea659688e73b57487397943618365383
Johan Lindstrom <jpl@nospam>
2011-03-01 20:17:49 +0000

    Source::DBIS usable

--- source/lib/App/Pebble/Plugin/Source/DBIS.pm
+++ source/lib/App/Pebble/Plugin/Source/DBIS.pm
@@ -15,6 +15,7 @@ use Moose;
 
 use Method::Signatures;
 
+use App::Pebble::Config;
 use DBIx::Simple;
 
 =head1 METHODS
@@ -29,8 +30,29 @@ $datasource_or_connect_string can be either a data source defined in
 
 =cut
 
-method db($class: $datasource_or_connect_string, $username?, $password? ) {
+my $connected_db;
+method db($class: $datasource_or_connect_string?, $username?, $password? ) {
+    my $docs = $datasource_or_connect_string or do {
+        $connected_db or die( "S::DBIS->db: ->db() called without connecting to a database beforehand \n" );
+        return $connected_db;
+    };
     
+    my $config = App::Pebble::Config->instance;
+    my $datasource;
+    my $connect;
+    my $config_section = "datasource_$docs";
+    if( $datasource = $config->{ $config_section } ) {
+        defined ( $connect = $datasource->{connect} )
+            or die( "S::DBIS->db: No 'connect' defined in the config section '[datasource_$docs].'\n" );
+        $username ||= $datasource->{username};
+        $password ||= $datasource->{password};
+    }
+    else {
+        $docs =~ /^dbi:/i or die( "S::DBIS->db: [$config_section] is not defined in the config, and does not look like a DBI connect string.\n" );
+        $connect = $docs;
+    }
+
+    $connected_db = DBIx::Simple->connect( $connect, $username, $password ) or die DBIx::Simple->error;
 }
 
 1;
