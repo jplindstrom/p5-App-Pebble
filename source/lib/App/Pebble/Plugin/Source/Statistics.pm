968375cd66c5eea9057056d0b294f21356558aa3
Johan Lindstrom <jpl@nospam>
2011-03-02 17:21:26 +0000

    S::Statistics->oadd_scale

--- source/lib/App/Pebble/Plugin/Source/Statistics.pm
+++ source/lib/App/Pebble/Plugin/Source/Statistics.pm
@@ -11,6 +11,10 @@ package App::Pebble::Plugin::Source::Statistics;
 use Moose;
 
 use Method::Signatures;
+use List::Util qw/ max /;
+
+use App::Pebble::Modifier::Pipeline;
+use Pebble::Object::Class;
 
 =head1 METHODS
 
@@ -38,13 +42,63 @@ Can also be used with only one attribute
 =cut
 
 method percent_of($class: $what_attribute, $percent_of_attributes ) {
-  ref $percent_of_attributes eq "ARRAY" or $percent_of_attributes = [ $percent_of_attributes ];
+    ref $percent_of_attributes eq "ARRAY" or $percent_of_attributes = [ $percent_of_attributes ];
 
-  my $pebble = $_;
-  return
-    map { ( "${_}_pct"  => $pebble->$_  / ( $pebble->$what_attribute || 1 ) ) }
-    @$percent_of_attributes;
+    my $pebble = $_;
+    return
+        map { ( "${_}_pct"  => $pebble->$_  / ( $pebble->$what_attribute || 1 ) ) }
+        @$percent_of_attributes;
 }
 
+=head2 oadd_scale($class: $of_attribute, :$new_attribute = $of_attribute, :$new_max_value = 10 ) : $pipeline_segment
+
+Return an oadd {} pipeline segment which adds a new attribute with a
+value scaled up or down to $new_max_value.
+
+    onew { temperature => int( rand( 500 )) }
+    | S::Statistics->oadd_scale( "temperature", "temp_norm", 30 )
+
+=cut
+
+method oadd_scale($class: $of_attribute, :$new_attribute = $of_attribute, :$new_max_value = 10 ) {
+    my @pebbles;
+    # validate $of_attribute
+
+    ###TODO: use data types to deal with ints
+
+    my $max_value;
+    ppool(
+        sub {
+            push( @pebbles, $_ );
+            my $value = $_->$of_attribute;
+            if( defined $max_value ) {
+                $max_value = max( $max_value, $value );
+            }
+            else {
+                $max_value = $value;
+            }
+            return ();
+        },
+        sub {
+            my $scale = $new_max_value / $max_value;
+
+            my @scaled_pebbles;
+            while( my $pebble = shift @pebbles ) {
+                push(
+                    @scaled_pebbles,
+                    Pebble::Object::Class->modify(
+                        -object => $pebble,
+                        -add    => {
+                            ###TODO: not always int()
+                            $new_attribute => int( $pebble->$of_attribute * $scale ),
+                        },
+                    ),
+                );
+            }
+
+            return @scaled_pebbles;
+        }
+    );
+}
 
 1;
